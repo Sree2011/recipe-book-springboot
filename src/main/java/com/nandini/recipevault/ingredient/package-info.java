f711a75d719f1dffe338e3ca4ada94c43c70927d
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2026-02-14 20:09:41 +0530

    add docs 3

---
+++ src/main/java/com/nandini/recipevault/ingredient/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Provides classes and interfaces for managing ingredients
+ * in the Recipe Vault application.
+ *
+ * <p>This package includes:
+ * <ul>
+ *   <li>Ingredient entity</li>
+ *   <li>Ingredient DTOs</li>
+ *   <li>Ingredient service and repository</li>
+ * </ul>
+ *
+ * Usage:
+ * <pre>
+ * Ingredient sugar = new Ingredient("Sugar", "2 tbsp");
+ * ingredientService.save(sugar);
+ * </pre>
+ */
+package com.nandini.recipevault.ingredient;
