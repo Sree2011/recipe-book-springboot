ffda1921e7343b19e6860d683c938352faabc0a9
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2026-04-23 17:55:16 +0530

    Fresh start: Java 25 Microservices Monorepo

---
+++ scaling-service/src/main/java/com/sai/fullstack/scaling_service/ScalingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sai.fullstack.scaling_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalingServiceApplication.class, args);
+	}
+
+}
---
+++ scaling-service/src/test/java/com/sai/fullstack/scaling_service/ScalingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.fullstack.scaling_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
